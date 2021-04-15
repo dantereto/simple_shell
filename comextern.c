97e2f28890cca2cf43892246bcf302d032469f6e
ForeroAlexander <foreroalex26@gmail.com>
2021-04-15 03:58:36 +0000

    comandos externos

--- comextern.c
+++ comextern.c
@@ -7,39 +7,39 @@
  */
 int comExterno(char *buff, char *main_tokens[])
 {
-  struct stat st;
-  int i = 0, exec_status = -1;
-  char *_envpath = NULL, *delim = ":", **path_tokens = '\0';
+	struct stat st;
+	int i = 0, exec_status = -1;
+	char *_envpath = NULL, *delim = ":", **path_tokens = '\0';
 
-  _memset(buff, '\0', 100);
-  _envpath = _getenv2("PATH1");
-  path_tokens = _strtok(_envpath, delim);
-  while (exec_status == -1 && path_tokens[i] != '\0')
-    {
-      _strcpy(buff, path_tokens[i]);
-      _strcat_extern(buff, main_tokens[0]);
-      if ((stat(buff, &st)) == 0 || (stat(main_tokens[0], &st)) == 0)
+	_memset(buff, '\0', 100);
+	_envpath = _getenv2("PATH1");
+	path_tokens = _strtok(_envpath, delim);
+	while (exec_status == -1 && path_tokens[i] != '\0')
 	{
-	  wait(&exec_status);
-	  if ((stat(main_tokens[0], &st)) == 0)
-	    exec_status = _execve(main_tokens[0], main_tokens, NULL);
-	  else
-	    exec_status =  _execve(buff, main_tokens, NULL);
+		_strcpy(buff, path_tokens[i]);
+		_strcat_extern(buff, main_tokens[0]);
+		if ((stat(buff, &st)) == 0 || (stat(main_tokens[0], &st)) == 0)
+		{
+			wait(&exec_status);
+			if ((stat(main_tokens[0], &st)) == 0)
+				exec_status = _execve(main_tokens[0], main_tokens, NULL);
+			else
+		  exec_status =  _execve(buff, main_tokens, NULL);
 
-	  free((void *)path_tokens[0]);
-	  free(path_tokens);
+			free((void *)path_tokens[0]);
+			free(path_tokens);
+		}
+
+		i++;
 	}
-
-      i++;
-    }
-  if (exec_status == 0)
-    return (0);
-  else if (exec_status == -1)
-    {
-      free((void *)path_tokens[0]);
-      free(path_tokens);  /* libera el doble puntero */
-      return (-1);   /* retorna el error */
-    }
-  else
-    exit(128); /* error no identificado error fatal */
+	if (exec_status == 0)
+		return (0);
+	else if (exec_status == -1)
+	{
+		free((void *)path_tokens[0]);
+		free(path_tokens);  /* libera el doble puntero */
+		return (-1);   /* retorna el error */
+	}
+	else
+		exit(128); /* error no identificado error fatal */
 }
