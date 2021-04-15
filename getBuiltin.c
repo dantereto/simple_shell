0e18aa8b1e083f9d59a7745f4f78bb25c2fed10d
ForeroAlexander <foreroalex26@gmail.com>
2021-04-15 04:03:03 +0000

    builtins

--- getBuiltin.c
+++ getBuiltin.c
@@ -8,18 +8,18 @@
  */
 int get_builtins(char *buffer,  char *main_tokens[], int salida)
 {
-  int i = 0;
+	int i = 0;
 
-  builtins_t build[] = {
-    {"env", _env},
-    {"exit", _exito},
-    {NULL, NULL}};
-  for (i = 0; build[i].function; i++)
-    {
-      if (_strcmp(buffer, build[i].function) == 0)
+	builtins_t build[] = {
+		{"env", _env},
+		{"exit", _exito},
+		{NULL, NULL}};
+	for (i = 0; build[i].function; i++)
 	{
-	  return (build[i].f(buffer, main_tokens, salida));
+		if (_strcmp(buffer, build[i].function) == 0)
+		{
+			return (build[i].f(buffer, main_tokens, salida));
+		}
 	}
-    }
-  return (-1);
+	return (-1);
 }
