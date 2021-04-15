260931400b1518abff6b9edac987a6b513e87e9a
dantereto <2247@holbertonschoolc.com>
2021-04-15 03:45:30 +0000

    get the builtins

---
+++ getBuiltin.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * get_builtins - search the sign
+ *@buffer: comando
+ *@main_tokens: tokens del main
+ *@salida: codigo de salida
+ *Return: return 0
+ */
+int get_builtins(char *buffer,  char *main_tokens[], int salida)
+{
+  int i = 0;
+
+  builtins_t build[] = {
+    {"env", _env},
+    {"exit", _exito},
+    {NULL, NULL}};
+  for (i = 0; build[i].function; i++)
+    {
+      if (_strcmp(buffer, build[i].function) == 0)
+	{
+	  return (build[i].f(buffer, main_tokens, salida));
+	}
+    }
+  return (-1);
+}
